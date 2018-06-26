6e150f7731d1ab77c1839d6c83e7df639755e6e6
avivharuzi <avivharuzi@gmail.com>
2018-06-26 13:56:41 +0300

    added basic home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <section>
+                <p>
+                    home works!
+                </p>
+            </section>
+        );
+    }
+}
+
+export default Home;
