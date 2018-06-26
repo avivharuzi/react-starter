49e4b94348afa0c8bd6857da2a7c905e06620ce0
avivharuzi <avivharuzi@gmail.com>
2018-06-26 16:47:34 +0300

    added page not found

---
+++ src/pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ErrorPage extends Component {
+    render() {
+        return (
+            <section>
+                <p>
+                    error-page works!
+                </p>
+            </section>
+        );
+    }
+}
+
+export default ErrorPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './../pages/Home/Home';
+import ErrorPage from './../pages/ErrorPage/ErrorPage';
 
 const routes = () => {
     return (
         <main>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route component={ErrorPage} />
             </Switch>
         </main>
     );
