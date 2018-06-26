7b8e702d05952473b2c3b89c16b0d4fa1e5ac58f
avivharuzi <avivharuzi@gmail.com>
2018-06-26 16:30:39 +0300

    added switch to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './../pages/Home/Home';
 
 const routes = () => {
     return (
         <main>
-            <Route path="/" exact component={Home} />
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
         </main>
     );
 };
