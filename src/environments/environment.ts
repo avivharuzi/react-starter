106492c8e355eaab1ae60df9ce5e3fb12ceac7d1
Aviv Haruzi <avivharuzi@gmail.com>
2020-10-08 11:53:37 +0300

    Update environment variables in code support

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,18 @@
+import prod from './environment.prod';
+import { Environments } from './environments';
+
 export interface Environment {}
 
-const defaultEnvironment: Environment = {};
+let environment: Environment = {};
 
 const stage: string | undefined = process.env.REACT_APP_STAGE;
 
-export const environment = stage !== undefined ? import(`environment.${stage}`) : defaultEnvironment;
+switch (stage) {
+  case Environments.Prod: {
+    environment = prod;
+  }
+}
+
+export {
+  environment,
+};
